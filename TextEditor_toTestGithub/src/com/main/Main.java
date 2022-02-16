e902640ad3413638e6b391d9d05b85d2e9c02537
AM2K-MTA <miqueias.thomaz@gmail.com>
2022-02-16 20:13:37 -0300

    Add: 1st add proj files to github

---
+++ TextEditor_toTestGithub/src/com/main/Main.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class Main {
+
+	/** Implementation wanted:
+	* 1_ Title: "Learn How to Create a TextEditor with Java - Intro", Link: https://youtu.be/os5HcwzKg5Q
+	* 2_ Title: "Java Text Editor (Software)", Link: https://youtu.be/ofv-yoz5lfw		(Has a lot of thing that BroCode doesn't made)
+	**/
+	
+	public static void main(String[] args) {
+		new TextEditor();
+	}
+
+}
